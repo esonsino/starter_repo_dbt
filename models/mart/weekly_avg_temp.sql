197f7ec5fd5c6e48ac5a6bd20ac7670c95bcdc52
esonsino <eytan.sonsino@gmail.com>
2024-01-18 11:02:51 +0100

    weekly

---
+++ models/mart/weekly_avg_temp.sql
@@ -0,0 +1,14 @@
+WITH total_avg AS (
+    SELECT
+        city,
+        country,
+        year,
+        lat,
+        lon,
+        AVG(avgtemp_c) AS avg_temp_week,
+        MAX(maxtemp_c) AS max_temp_week,
+        MIN(mintemp_c) AS min_temp_week
+        FROM {{ref("prep_temp")}}
+        GROUP BY city, country, year, week, lat, lon
+)
+SELECT * FROM total_avg
